f4d6ae31af94b57d6c525beb76dfd3b7d91ec079
Michail Gilev <mishagilevfgh@e1.ru>
2022-03-02 23:49:19 +0300

    Осталось вычислить всякие погрешности

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -1,13 +1,60 @@
+#include <QCoreApplication>
 #include <iostream>
 #include <vector>
 #include <math.h>
 #include <iomanip>
+#include <fstream>
+#include <QTextStream>
+#include <QFile>
+#include <QDebug>
 
 #define n_rows 4
 #define n_cols 3
 
 using namespace std;
 
+const QString allFileToString(QFile &aFile)
+{
+    if (!aFile.open(QFile::ReadOnly | QFile::Text)) {
+        std::cout << "Error opening file!" << std::endl;
+        return NULL;
+    }
+    QTextStream in(&aFile);
+    return in.readAll();
+}
+
+void setDataToVector(const QStringList &aStringList,
+                     vector< vector <double> > &aVector)
+{
+    for (size_t row = 0;row < (aStringList.size() - 1);row++)
+    {
+        QStringList stroka = aStringList.at(row).split(" ");
+        vector<double> simple;
+        for (size_t i =0;i<stroka.size();i++)
+        {
+            if ((stroka[i] != "") && (stroka[i] != "\t"))
+            {
+                simple.push_back(stroka[i].toDouble());
+            }
+        }
+        aVector.push_back(simple);
+    }
+}
+
+void printVector(const QVector< QVector <double> > &aVector)
+{
+    for(auto i=aVector.begin();i != aVector.end();i++)
+    {
+        for(auto j=i->begin();j != i->end();j++)
+        {
+            cout <<scientific << setw(20) << *j;
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void sum(vector<vector<double>> &matrix1,vector<vector<double>> matrix2);
 void printMatrix(vector<vector<double>> matrix);
 void printMatrix(vector<double> vector);
 void expanded_matrix(vector<vector<double>> &matrix,vector<double> vector);
@@ -19,101 +66,87 @@ vector<double> operator*(const vector<double>&a, double constanta);
 int col_max(vector<vector<double>> &matrix,int col);
 int triangulation(vector<vector<double>> &matrix);
 double determinant(vector<vector<double>> matrix);
-vector<vector<double>> matrix_multiplication(vector<vector<double>> &m1,
-                                             vector<vector<double>> &m2);
+vector<vector<double>> matrix_multiplication(vector<vector<double>> m1,
+                                             vector<vector<double>> m2);
 void transpose(vector<vector<double>> &matrix);
 void scalar_multi(vector<vector<double>> &matrix, double factor);
-vector<vector<double>> inverse_matrix(vector<vector<double>> &matrix);
+vector<vector<double>> inverse_matrix(vector<vector<double>> matrix);
 double N_1(vector<vector<double>> &matrix);
 double N_inf(vector<vector<double>> &matrix);
+void Get_matrix(vector<vector<double>> matrix, vector<vector<double>> &tmp,int indRow, int indCol);
 
 int main()
 {
-    vector<vector<double>> matrix{{1,2,3},     //(5, vector<int>(3,0));
-                                  {4,5,6}};
+    vector<vector<double>> matrix1{{-0.0660,0.6600,1.0050,-6.6900},     //(5, vector<int>(3,0));
+                                  {-3.8640,7.2080,47.5740,-292.0740},
+                                  {-23.1840,42.9840,285.4450,-1752.1860},
+                                  {-3.8640,7.1640,47.5740,-292.0300}};
 
-    vector<vector<double>> matrix2{ {7,8},     //(5, vector<int>(3,0));
-                                    {9,1},
-                                    {2,3}};
+    vector<double> b{-46.2300,-2219.2580,-13315.8580,-2219.3020};
 
-    vector<double> b{-83.32,38.9,-56.7};
+    expanded_matrix(matrix1,b);
 
-//    matrix.resize(n_rows);
+    printMatrix(matrix1);
 
-//    for (int i=0;i != matrix.size();i++)
-//    {
-//        matrix[i].resize(n_cols);
-//    }
+    vector< vector <double> > matrix;
+    QFile file("C:/Users/Миша/Desktop/Проверка.txt");
 
-    //expanded_matrix(matrix,b);
-
-    //printMatrix(matrix);
-
-    //double n = determinant(matrix);
-
-    //cout << n << endl;
-
-    //printMatrix(matrix);
+    setDataToVector(allFileToString(file).split("\n"), matrix);
 
     printMatrix(matrix);
 
-    transpose(matrix);
+    size_t number_of_row = 0; //для того, чтобы спускаться по уровням матрицы
+                              //например для того, чтобы перейти с матрицы 3x3
+                              //на матрицу уровня 2x2 и т.д.
+
+    while (number_of_row < matrix.size())
+    {
+        max_in_column(matrix,number_of_row);
+
+        double a_ii = (double)matrix[number_of_row][number_of_row];
+
+        for(size_t column=number_of_row; column < matrix[number_of_row].size();column++)
+        {
+            matrix[number_of_row][column] = (double)matrix[number_of_row][column]/a_ii;//нормировка текущей строки
+//            double a_i1 = (double)matrix[row][number_of_row];
+//            for(size_t column =number_of_row;column<matrix[row].size();column++)
+//            {
+//                matrix[row][column] = (double)matrix[row][column]/a_i1;
+//            }
+        }
+
+        for(size_t row =(number_of_row+1); row < matrix.size();row++)
+        {
+            double divider = -1*matrix[row][number_of_row];
+            vector<double>temp = operator*(matrix[number_of_row],divider);
+            matrix[row] = operator+(matrix[row],temp);
+        }
+        number_of_row++;
+    }
 
     printMatrix(matrix);
 
+    //обратный ход
 
-//    size_t number_of_row = 0; //для того, чтобы спускаться по уровням матрицы
-//                              //например для того, чтобы перейти с матрицы 3x3
-//                              //на матрицу уровня 2x2 и т.д.
+    vector<double> answer(matrix.size()); //задаем вектор ответов размера количества строк в матрице.
 
-//    while (number_of_row < matrix.size())
-//    {
-//        max_in_column(matrix,number_of_row);
+    //k_i = (b_i - sum( от j = k+1 до n = matrix.size())(a_ij*k_j))
 
-//        double a_ii = (double)matrix[number_of_row][number_of_row];
+    double sum;
+    double product; //произведение коэффициента матрицы на число вектора ответов
 
-//        for(size_t column=number_of_row; column < matrix[number_of_row].size();column++)
-//        {
-//            matrix[number_of_row][column] = (double)matrix[number_of_row][column]/a_ii;//нормировка текущей строки
-////            double a_i1 = (double)matrix[row][number_of_row];
-////            for(size_t column =number_of_row;column<matrix[row].size();column++)
-////            {
-////                matrix[row][column] = (double)matrix[row][column]/a_i1;
-////            }
-//        }
+    for(int k = (matrix.size()-1); k >= 0;k--) //индексы обхода матрицы
+    {
+        sum = 0; //каждый раз обнуляем сумму
+        for (size_t j =k+1; j <= (matrix.size()-1);j++)
+        {
+            product = matrix[k][j] * answer[j];
+            sum = sum + product;
+        }
+        answer[k] = (matrix[k][(matrix[k].size()-1)] - sum);    //matrix[k][k];
+    }
 
-//        for(size_t row =(number_of_row+1); row < matrix.size();row++)
-//        {
-//            double divider = -1*matrix[row][number_of_row];
-//            vector<double>temp = operator*(matrix[number_of_row],divider);
-//            matrix[row] = operator+(matrix[row],temp);
-//        }
-//        number_of_row++;
-//    }
-
-//    printMatrix(matrix);
-
-//    //обратный ход
-
-//    vector<double> answer(matrix.size()); //задаем вектор ответов размера количества строк в матрице.
-
-//    //k_i = (b_i - sum( от j = k+1 до n = matrix.size())(a_ij*k_j))
-
-//    double sum;
-//    double product; //произведение коэффициента матрицы на число вектора ответов
-
-//    for(int k = (matrix.size()-1); k >= 0;k--) //индексы обхода матрицы
-//    {
-//        sum = 0; //каждый раз обнуляем сумму
-//        for (size_t j =k+1; j <= (matrix.size()-1);j++)
-//        {
-//            product = matrix[k][j] * answer[j];
-//            sum = sum + product;
-//        }
-//        answer[k] = (matrix[k][(matrix[k].size()-1)] - sum);    //matrix[k][k];
-//    }
-
-//    printMatrix(answer);
+    printMatrix(answer);
 
     return 0;
 }
@@ -135,7 +168,7 @@ void printMatrix(vector<double> vector)
 {
     for (auto it = vector.begin(); it!=vector.end();it++)
     {
-        cout << setw(20) << *it << endl;
+        cout << scientific <<setprecision(15) <<setw(30) << *it << endl;
     }
     cout << endl;
 }
@@ -257,8 +290,8 @@ double determinant(vector<vector<double>> matrix)
     return determinant;
 }
 
-vector<vector<double>> matrix_multiplication(vector<vector<double>> &m1,
-                                             vector<vector<double>> &m2)
+vector<vector<double>> matrix_multiplication(vector<vector<double>> m1,
+                                             vector<vector<double>> m2)
 {
     vector<vector<double>> answer;
     answer.resize(m1.size());
@@ -316,12 +349,11 @@ void scalar_multi(vector<vector<double>> &matrix, double factor)
 double N_1(vector<vector<double>> &matrix)
 {
     double N1 = 0;
-    for(size_t row = 0; row<matrix.size();row++)
+    for(size_t row = 0; row<matrix[0].size();row++)
     {
-        double colsum =0, rowsum =0;
-        for(size_t col = 0;col<matrix[0].size();col++)
+        double colsum =0;
+        for(size_t col = 0;col<matrix.size();col++)
         {
-            rowsum += abs(matrix[row][col]);
             colsum += abs(matrix[col][row]);
         }
         N1 = max(colsum,N1);
@@ -334,11 +366,10 @@ double N_inf(vector<vector<double>> &matrix)
     double Ninf = 0;
     for(size_t row = 0; row<matrix.size();row++)
     {
-        double colsum =0, rowsum =0;
+        double rowsum =0;
         for(size_t col = 0;col<matrix[0].size();col++)
         {
             rowsum += abs(matrix[row][col]);
-            colsum += abs(matrix[col][row]);
         }
         Ninf = max(rowsum,Ninf);
     }
@@ -347,49 +378,55 @@ double N_inf(vector<vector<double>> &matrix)
 
 vector<vector<double>> inverse_matrix(vector<vector<double>> matrix)
 {
-    vector<vector<double>> copy = matrix;
-    //алгоритм Ньютона-Шульца
-    double N1 = N_1(matrix); // единичная норма
-    double Ninf = N_inf(matrix);
-
-    transpose(matrix);
-
-    scalar_multi(matrix,(1/(N1*Ninf))); //нормирование матрицы
-
-    vector<vector<double>> E2;
-
-    E2.resize(matrix.size());
-
-    for(size_t i = 0;i<E2.size();i++)
+    vector<vector<double>> answer;
+    answer.resize(matrix.size());
+    for(size_t i =0;i<answer.size();i++)
     {
-        E2[i].resize(matrix[0].size());
+        answer[i].resize(matrix[0].size());
     }
 
-    for(size_t row = 0; row<E2.size();row++)
+    vector<vector<double>> tmp;
+
+    double det = determinant(matrix);
+
+    for(size_t row = 0; row<matrix.size();row++)
     {
-        for(size_t col = 0;col<E2[0].size();col++)
+        for(size_t col = 0;col<matrix[0].size();col++)
         {
-            if(row==col)
-                E2[row][col] = 2;
-            else
-                E2[row][col] = 0;
+            Get_matrix(matrix,tmp,row,col);
+            answer[row][col] = ((pow(-1.0,(row+col+2))*determinant(tmp))/det);
         }
     }
-    vector<vector<double>> inv = matrix;
-    vector<vector<double>> tmp;
-    double EPS = 0.001; //погрешность
-
-    while ((abs(determinant(matrix_multiplication(copy,inv)))-1) >= EPS)
-    {
-
-    }
+    transpose(answer);
+    return answer;
 }
 
+void sum(vector<vector<double>> &matrix1,vector<vector<double>> matrix2)
+{
+    for(size_t row = 0; row < matrix1.size(); row++)
+            for(size_t col = 0; col < matrix1[0].size(); col++)
+                matrix1[row][col] += matrix2[row][col];
+}
 
+void Get_matrix(vector<vector<double>> matrix, vector<vector<double>> &tmp,int indRow, int indCol)
+{
+    tmp.resize(matrix.size()-1);
 
+    for(size_t i = 0;i<tmp.size();i++)
+    {
+        tmp[i].resize(matrix[0].size()-1);
+    }
 
-
-
-
-
-
+    int ki = 0;
+    for (int row = 0; row < matrix.size(); row++){
+        if(row != indRow){
+            for (int col = 0, kj = 0; col < matrix.size(); col++){
+                if (col != indCol){
+                    tmp[ki][kj] = matrix[row][col];
+                    kj++;
+                }
+            }
+            ki++;
+        }
+    }
+}
